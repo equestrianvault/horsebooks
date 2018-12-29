1c8d226dbe084f2410554972368fb4751aeb0c70
Nathan Brown <nrb62281@gmail.com>
2018-12-28 19:05:59 -0800

    Heroku deploy tweak

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express'),
 app = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/horsebooks"));
 app.get('/', function(req, res) {
-    res.sendfile('index.html', {root: __dirname + "/dist" })
+    res.sendfile('index.html', {root: __dirname + "/dist/horsebooks" })
 });
 var server = app.listen(process.env.PORT || 80);
