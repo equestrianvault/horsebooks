2d9ff25c053f0ae2d0b24a43f062afa23c8199b9
Nathan Brown <nrb62281@gmail.com>
2019-01-09 23:36:38 -0800

    Switching from heroku to netlify, and slight rebrand to crystal archives

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-app = express();
-
-app.use(express.static(__dirname + "/dist/horsebooks"));
-app.get('/', function(req, res) {
-    res.sendfile('index.html', {root: __dirname + "/dist/horsebooks" })
-});
-var server = app.listen(process.env.PORT || 80);
--- src/app/components/heading/heading.component.ts
+++ src/app/components/heading/heading.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./heading.component.css']
 })
 export class Heading {
-  title = 'Horse Books';
+  title = 'Crystal Archives';
 }
