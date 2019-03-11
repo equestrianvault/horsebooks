c09190ce26ce1fd04a9881e65d5f0148c0e96619
KeystrokeCascade <haydenseen1@gmail.com>
2019-03-11 11:10:24 +1100

    Updated missing image

--- src/app/components/horse-books-listing/horse-books-listing.component.ts
+++ src/app/components/horse-books-listing/horse-books-listing.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { HorseBooksService, Book } from '../../services/books.service';
 import { SearchPipe } from '../../search.pipe';
-import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'horse-books-listing',
@@ -9,23 +8,12 @@ import { ActivatedRoute } from "@angular/router";
   styleUrls: ['./horse-books-listing.component.scss'],
   providers: [HorseBooksService]
 })
-export class HorseBooksListing implements OnInit{
+export class HorseBooksListing{
 	books : Book[] = null;
 	searchVal : string = "";
 	searchValPlaceholder : string = "Search";
+	showExpired : Boolean = false;
 	showMature : Boolean = false;
-	fanficOnly : Boolean = true;
-	
-	ngOnInit() {
-		this.route.queryParams.subscribe(params => {
-		    if(Object.prototype.hasOwnProperty.call(params, "showExpired")){
-				this.showExpired = !!params.showExpired;
-			}else{
-				this.showExpired = false;
-			}
-		});
-	}
-
 	
 	constructor(private bookService: HorseBooksService, private route: ActivatedRoute){
 		this.books = bookService.getBooks();
