b9b75bf0db3f647bca03eae483929509089b9383
Nathan Brown <tsitra@users.noreply.github.com>
2022-01-28 15:50:46 -0800

    Pointing to a feature branch for data

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -11,7 +11,7 @@ export class HorseBooksService {
 	getBooks() {
 		return this
 		.http
-		.get("https://raw.githubusercontent.com/equestrianvault/horsebooks-data/main/data/books.json");
+		.get("https://raw.githubusercontent.com/equestrianvault/horsebooks-data/blob/feature-anthologies/data/books.json");
 	}
 }
 
