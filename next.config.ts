eeee57b55452b499559bdb4fa30388c6f61ce8c4
Nathan Brown <nathan@nathanrbrown.dev>
2025-11-22 23:18:19 -0800

    Fixing build issues on netlify

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export',
+  assetPrefix: './',
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
