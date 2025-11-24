3716ba8641c85f2502f644a5a42dbe72edb11c7d
Nathan Brown <nathan@nathanrbrown.dev>
2025-11-23 19:13:07 -0800

    Dynamically loading data from a netlify function

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,51 @@
 import type { NextConfig } from "next";
 
+const domains: Array<string> = [
+
+];
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'export',
-  assetPrefix: './',
-  images: { unoptimized: true },
+  // output: 'export',
+  // assetPrefix: './',
+  // images: { unoptimized: true },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn-img.fimfiction.net",
+      },
+      {
+        protocol: "https",
+        hostname: "images-wixmp-*.wixmp.com",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.lulu.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.etsystatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+      },
+      {
+        protocol: "https",
+        hostname: "bookshow.blurb.com",
+      },      
+      {
+        protocol: "https",
+        hostname: "assets.bigcartel.com",
+      },
+      {
+        protocol: "https",
+        hostname: "derpicdn.net",
+      },
+    ]
+  }
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,49 @@
 "use client";
-import { AppBar, Stack, Typography } from "@mui/material";
+
+import { AppBar, Card, CardContent, CardHeader, CardMedia, Grid, List, ListItem, Stack, Typography } from "@mui/material";
 import Image from "next/image";
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+
+const BOOK_DATA_URL = "/.netlify/functions/books";
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+interface IBook{
+  id : number;
+	title : string;
+	edition : string;
+	img : string;
+	dateAdded? : string;
+	authors : Array<IAuthor>;
+	links : Array<IBookLink>;
+	tags? : Array<string>;
+	rating? : string;
+	expiry? : string;
+}
+
+interface IBookLink{
+	title : string;
+	url : string;
+}
+
+interface IAuthor{
+	name : string;
+	url? : string;
+}
+
+interface FetchResponseProps {
+  onReturned?: Response,
+  onRejected?: any
+}
+
 
 export default function Home() {
+  const { data, error, isLoading } = useSWR(BOOK_DATA_URL, fetcher);
+  if (error) return <Typography>{error}</Typography>
+  if (isLoading) return <Typography>Loading...</Typography>;  
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-12xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <AppBar sx={{backgroundColor: "rgb(63, 81, 181)"}} >
           <Stack direction={"row"} py={{md:3, sm: 1}} px={{md:2, sm:1}} spacing={2}>
             <Typography fontSize={"20px"} >Equestrian Vault</Typography>
@@ -17,62 +55,78 @@ export default function Home() {
               <Image width={20} height={30} src={"window.svg"} alt={"window icon"}/>
             </Stack>
           </Stack>
-        </AppBar>
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+        </AppBar>        
+        <Grid 
+          container 
+
+          columns={{
+            xl: 8,
+            lg: 6,
+            md: 4,
+            sm: 3,
+            xs: 1
+          }} 
+
+          spacing={2} 
+
+          height={{
+            xl: 400,
+            lg: 600,
+            md: 600,
+            sm: 600,
+            xs: 600
+          }}
+          maxWidth={{
+            xl: 1436, 
+            lg: 1100, 
+            md: 800, 
+            sm: 500,
+            xs: 1
+          }}
+          minWidth={{
+            xl: 1436, 
+            lg: 1100, 
+            md: 800, 
+            sm: 500,
+            xs: 1
+          }}
+
+          marginX="auto"
+        >
+          {data.books.map((book: IBook, index) =>(
+            <Grid key={index} size={1} height={1}>
+              <Card sx={{height: "100%", width: "100%", overflow: "clip"}}>
+                <CardHeader disableTypography={false} title={book.title} slotProps={{title: {
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  overflow: "clip",
+                }}}></CardHeader>
+                <CardContent sx={{overflow: "clip"}}>
+                    <List disablePadding dense subheader="By:" >
+                      {book.authors.map((author: IAuthor, authorIndex) => (
+                        <ListItem disableGutters disablePadding divider key={authorIndex} dense>
+                          {author.name}
+                        </ListItem>
+                      ))}
+                    </List>
+                </CardContent>
+                <CardMedia>
+                  <Image 
+                    src={"window.svg"}
+                    width={1}
+                    height={1}
+                    style={{
+                      width: "100%",
+                      height: "auto"
+                    }}
+                    alt={book.title} >
+                  </Image>
+                </CardMedia>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+
       </main>
     </div>
   );
